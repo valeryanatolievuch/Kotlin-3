bb920bc3b54470ca9e4ee087772413400f4143bc
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-13 12:02:57 +0200

    Function with return type

--- src/main/kotlin/ru/otus/homework/functions.kt
+++ src/main/kotlin/ru/otus/homework/functions.kt
@@ -1,12 +1,14 @@
 package ru.otus.homework
 
 fun main() {
-    calculate(10, 20)
+    val s1: String = calculate(10, 20)
+    println(s1)
+
     calculate(30, 40)
     calculate(50, 60)
 }
 
-fun calculate(n1: Int, n2: Int) {
+fun calculate(n1: Int, n2: Int): String {
     val s = n1 + n2
-    println("$n1 + $n2 = $s")
+    return "$n1 + $n2 = $s"
 }

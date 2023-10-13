19dd17f08aef794abe150b8fae2891bac9346be0
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-13 10:58:41 +0200

    More additions

--- src/main/kotlin/ru/otus/homework/functions.kt
+++ src/main/kotlin/ru/otus/homework/functions.kt
@@ -1,8 +1,18 @@
 package ru.otus.homework
 
 fun main() {
-    val n1 = 10
-    val n2 = 20
-    val s = n1 + n2
+    var n1 = 10
+    var n2 = 20
+    var s = n1 + n2
+    println("$n1 + $n2 = $s")
+
+    n1 = 30
+    n2 = 40
+    s = n1 + n2
+    println("$n1 + $n2 = $s")
+
+    n1 = 50
+    n2 = 60
+    s = n1 + n2
     println("$n1 + $n2 = $s")
 }

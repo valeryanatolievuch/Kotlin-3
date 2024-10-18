ea1b15f3c7991792136637c5282752b45bd276d1
Валерий Асланов <valeryanatolievuch@gmail.com>
2024-10-18 20:58:59 +0500

    Задача 2: Функция для объединения строк с необязательным параметром

--- src/main/kotlin/ru/otus/homework/homework kotlin 3.kt
+++ src/main/kotlin/ru/otus/homework/homework kotlin 3.kt
@@ -1,7 +1,16 @@
 package ru.otus.homework
+
 fun sumOfNumbers(first: Int, second: Int, vararg others: Int): Int {
     return first + second + others.sum()
 }
+
+fun joinStrings(vararg strings: String, separator: Char = ' '): String {
+    return strings.joinToString(separator.toString())
+}
+
 fun main() {
-    println(sumOfNumbers(1,2,3,4,5))
+    println(sumOfNumbers(1, 2, 3, 4, 5))
+
+    println(joinStrings("str1", "str2", "str3"))
+    println(joinStrings("str1", "str2", "str3", separator = ','))
 }

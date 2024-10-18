108033ffb9987855c16cfb3c6b0e2b3b32d0eaa0
Валерий Асланов <valeryanatolievuch@gmail.com>
2024-10-18 21:08:55 +0500

    Задача 3: Тестовая функция для проверки joinStrings

--- src/main/kotlin/ru/otus/homework/homework kotlin 3.kt
+++ src/main/kotlin/ru/otus/homework/homework kotlin 3.kt
@@ -8,9 +8,28 @@ fun joinStrings(vararg strings: String, separator: Char = ' '): String {
     return strings.joinToString(separator.toString())
 }
 
+fun testJoinStrings() {
+    val result1 = joinStrings("str1", "str2", "str3")
+    val result2 = joinStrings("str1", "str2", "str3", separator = ',')
+
+    if (result1 == "str1 str2 str3") {
+        println("Тест 1 пройден")
+    } else {
+        println("Тест 1 провален")
+    }
+
+    if (result2 == "str1,str2,str3") {
+        println("Тест 2 пройден")
+    } else {
+        println("Тест 2 провален")
+    }
+}
+
 fun main() {
     println(sumOfNumbers(1, 2, 3, 4, 5))
 
     println(joinStrings("str1", "str2", "str3"))
     println(joinStrings("str1", "str2", "str3", separator = ','))
+
+    testJoinStrings()
 }
